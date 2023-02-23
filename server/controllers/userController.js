867e2f0b34cb6a513911fa065c12619d8563b8de
Michelle <tianw0420@gmail.com>
2023-02-22 18:21:33 -0800

    edited some things

---
+++ server/controllers/userController.js
@@ -0,0 +1,13 @@
+import bcrypt from 'bcrypt.js'; //use for passwords
+import jwt from 'jsonwebtoken'; //use for verification
+
+import User from '../models/users.js'; 
+import { roles } from '../roles.js';
+
+export const signin = async (req, res) => {
+
+}
+
+export const signup = async (req, res) => {
+    
+}
--- server/server.js
+++ server/server.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+
 require("dotenv").config({ path: "./vars/config.env" });
 const port = process.env.PORT || 9000;
+
 app.use(cors());
 app.use(express.json());
 app.use(require("./routes/record"));
